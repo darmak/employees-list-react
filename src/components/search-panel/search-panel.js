44d05c683372de4a5bd5eabdc7cf812188b04e21
Buraukin Dzmitry <buravkin08@gmail.com>
2021-10-26 12:42:58 +0300

    init:start task

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+
+import './search-panel.css';
+
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ""
+        }
+    }
+
+    onUpdateSeacrh = (e) => {
+        const term = e.target.value;
+        this.setState({term:term});
+        this.props.onUpdateSeacrh(term);
+    }
+
+    render() {
+        return (
+            <input 
+                type="text"
+                className="form-control search-input"
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSeacrh}/>
+        );
+    }
+}
+
+export default SearchPanel;
